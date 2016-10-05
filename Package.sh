c478bd07fbe231057c1122f4bc367747303ec445
miguel <ma.bernabecruz@gmail.com>
2016-10-05 17:36:46 +0200

    Fix minor error in script Package.sh

--- Package.sh
+++ Package.sh
@@ -3,4 +3,4 @@
 export PATH=/home/miguel/Descargas/apache-maven-3.3.9/bin:$PATH
 mvn clean -Denvironment.type=develop
 mvn test -Denvironment.type=develop
-mvn test -Denvironment.type=preproduction
+mvn package -Denvironment.type=preproduction
